a52267bcb089bf4c7b088ca54e804988ae7cb2df
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 23:38:10 +0000

    Adding numeric_owner as keyword arguement

--- prepare_model_training.py
+++ prepare_model_training.py
@@ -282,7 +282,7 @@ if not (os.path.exists(CHECKPOINT_PATH)):
               if not is_within_directory(path, member_path):
                   raise Exception("Attempted Path Traversal in Tar File")
       
-          tar.extractall(path, members, numeric_owner) 
+          tar.extractall(path, members, numeric_owner=numeric_owner) 
           
       
       safe_extract(tar, path=CHECKPOINT_PATH)
